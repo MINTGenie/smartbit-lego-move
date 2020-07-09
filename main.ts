aac2e9789da124ec524305443d9f405d20e60d82
MINT Genie <66748747+MINTGenie@users.noreply.github.com>
2020-07-09 19:12:21 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,20 @@
 input.onButtonPressed(Button.A, function () {
+    SuperBit.Music(SuperBit.enMusic.entertainer)
     if (speed < 255) {
         speed += 20
     }
-    updtservo(speed, 1)
+    updtservo(speed, speed)
 })
 input.onButtonPressed(Button.AB, function () {
-    updtservo(SPEEDNIL, 1)
+    SuperBit.Music(SuperBit.enMusic.prelude)
+    updtservo(SPEEDNIL, SPEEDNIL)
 })
 input.onButtonPressed(Button.B, function () {
+    SuperBit.Music(SuperBit.enMusic.dadadum)
     if (speed > -250) {
         speed += -20
     }
-    updtservo(speed, 1)
+    updtservo(speed, speed)
 })
 function updtservo (speed_left: number, speed_right: number) {
     SuperBit.MotorRunDual(
@@ -28,7 +31,12 @@ let pos = 35
 SPEEDNIL = 0
 speed = SPEEDNIL
 updtservo(speed, 1)
+let strip = neopixel.create(DigitalPin.P12, 4, NeoPixelMode.RGB)
+strip.setBrightness(10)
+strip.showRainbow(1, 360)
 basic.forever(function () {
+    strip.rotate(3)
+    strip.show()
     while (pos < 55) {
         SuperBit.Servo(SuperBit.enServo.S2, pos)
         pos += 5
